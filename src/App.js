551034919efa165d5724addf8e164864553612a7
binayaksadangi <youbinayak@gmail.com>
2022-10-13 17:48:46 +0530

    Routes added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,44 @@ import Categories from "./components/Categories/Categories";
 import Contactus from "./components/ContactUS/Contactus";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
+import Register from "./components/Registration/Register";
 import Navbar from "./components/Navbar/Navbar";
-
+import CategoriesDetail from "./components/Categories/CategoriesDetail";
+import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Home/>
-    <About/>
-    <Categories/>
-    <Contactus/>
-    <Footer/>
+    <Routes>
+      <Route path="/" element={
+        <>
+        <Navbar/>
+        <Home/>
+        <About/>
+        <Categories/>
+        <Contactus/>
+        <Footer/>
+        </>
+      }/>
+      <Route path="/register" element=
+      {
+        <>
+          <Navbar/>
+          <Register/>
+        </>
+      }/>
+      <Route path="/categories/:id" element=
+      {
+        <>
+          <Navbar/>
+          <CategoriesDetail/>
+        </>
+      }/>
+        
+        
+    </Routes>
+    {/* 
+     */}
     </>
   );
 }
