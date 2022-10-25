73b38bee5636b70e3b6661553f9554f51b1d09e3
sugyani31 <manjari.dashsugyani305@gmail.com>
2022-10-25 11:31:42 -0700

    updated

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import './Home.css'
-import logo from '../../assets/image.jpg'
+import logo from '../../assets/hack.gif'
 const Home = () => {
   return (
     <div className="section">
       <div className="container">
         <div className="content-section">
           <div className="title">
-            <h1 className='hd1'>Hack-o-Fast</h1>
+            <h1 className='hd1'>INNOBUZZ</h1>
           </div>
           <div className="content">
-            <h2 className='subhed'>Think | Code | Win</h2>
+            <h2 className='subhed'>Ideate | Iterate | Culminate</h2>
             <p className='pd1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ipsam inventore quam placeat quibusdam a sed. Reprehenderit repudiandae dolorem impedit sunt iste ducimus, voluptate molestiae nulla tempora consequuntur, sed nemo quas similique facere quos asperiores sequi dolorum enim at, explicabo autem. Dolorem, placeat quis nam dolore iusto voluptas fugit porro recusandae accusantium ab explicabo, fuga iste, similique dolor nesciunt consectetur ex deleniti harum. Eveniet.</p>
             <div className="button">
               <a href=" ">Register</a>
@@ -21,7 +21,7 @@ const Home = () => {
           </div>
         </div>
         <div className="image-section">
-          <img className="img" src={logo} alt="hackathon" />
+          <img className="img" src={logo} alt="hackathon gif" />
         </div>
       </div>
     </div>
