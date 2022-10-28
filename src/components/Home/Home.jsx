f2e1d29461774e6e8ee89bd91b74093fbfdd68be
Anshu <anshuagrawal1901302@gmail.com>
2022-10-28 21:38:59 +0530

    some changes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
             <h1 className='hd1'>INNOBUZZ</h1>
           </div>
           <div className="content">
-            <h2 className='subhed'>Ideate | Iterate | Culminate</h2>
+            <h2 className='subhed' style={{color:'white'}}>Ideate | Iterate | Culminate</h2>
             <p className='pd1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ipsam inventore quam placeat quibusdam a sed. Reprehenderit repudiandae dolorem impedit sunt iste ducimus, voluptate molestiae nulla tempora consequuntur, sed nemo quas similique facere quos asperiores sequi dolorum enim at, explicabo autem. Dolorem, placeat quis nam dolore iusto voluptas fugit porro recusandae accusantium ab explicabo, fuga iste, similique dolor nesciunt consectetur ex deleniti harum. Eveniet.</p>
             <div className="button">
               <a href=" ">Register</a>
